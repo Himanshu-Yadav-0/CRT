c9a7cc9ce380266084adc9155dadda4227829f6b
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-19 18:36:26 +0530

    Question1 done

---
+++ Day1/question1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number =0;
+    cin>>number;
+    if(number&1==1)cout<<"number is odd";
+    else cout<<"number is even";
+    return 0;
+}
