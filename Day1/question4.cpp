9c04c11e77d08b4db67dde69b50bb44c5d2b3e18
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-19 18:56:01 +0530

    question4 done

---
+++ Day1/question4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the number of teams";
+    int number = 0;
+    cin>>number;
+    cout<<number-1<<" matches will be done for one winner";
+    return 0;
+}
