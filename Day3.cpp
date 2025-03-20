4c89bbc4064dd351c63058a059112f392ff1cdd5
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-20 11:34:56 +0530

    selection sort

--- Day3.cpp
+++ Day3.cpp
@@ -1,7 +1,39 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+void bubbleSort(vector<int>&arr){
+    int n = arr.size();
+    for(int i =0;i<n-1;i++){
+        bool flag = true;
+        for(int j =0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag = false;
+            }
+        }
+        if(flag) break;
+    }
+}
+
+void selectionSort(vector<int>&arr){
+    int n = arr.size();
+    for(int i = 0;i<n-1;i++){
+        int min_index =i;
+        for(int j = i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+
+}
+
 int main(){
-    int thisarray[5]={1,2,3};
-    cout<<thisarray[10];
+    vector<int>arr = {1,1,7,6,5};
+    selectionSort(arr);
+    for(int i =0;i<arr.size();i++){
+        cout<<arr[i]<<endl;
+    }
     return 0;
 }
