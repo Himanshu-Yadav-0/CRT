ec160da99193e5b25a371f634fd38cbb4219d237
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-22 09:35:56 +0530

    searching

--- Day4.cpp
+++ Day4.cpp
@@ -11,9 +11,24 @@ int linearSearch(vector<int>arr,int target){
     return -1;
 }
 
+int binarySearch(vector<int>arr, int target){
+    int low = 0;
+    int high = arr.size();
+    while(low<high){
+        int mid = (low+high)/2;
+        if(arr[mid]==target){
+            return mid;
+        }else if(arr[mid]>target){
+            high = mid-1;
+        }else
+            low = mid+1;
+    }
+    return -1;
+}
+
 int main(){
-    vector<int>arr = {2,4,5,1,3};
-    cout<<"Found at index: "<<linearSearch(arr,4);
+    vector<int>arr = {1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,100,120};
+    cout<<"Found at index: "<<binarySearch(arr,20);
 
     return 0;
 }
