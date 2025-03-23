102d8abfa5ac9986552ffc1e25f55ccfb90d7ad1
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-23 08:31:33 +0530

    Day4

--- Day4.cpp
+++ Day4.cpp
@@ -26,9 +26,39 @@ int binarySearch(vector<int>arr, int target){
     return -1;
 }
 
+int maxelem(vector<int>arr){
+    int max = arr[0];
+    int secondmax = arr[1];
+    if(arr.size()==1){
+        return max = arr[0];
+    }
+
+    if(secondmax>max){
+        swap(max,secondmax);
+    }
+    for(int i =2 ;i<arr.size();i++){
+        if(arr[i]>max){
+            secondmax = max;
+            max = arr[i];
+        }else if(secondmax <arr[i] && arr[i]!=max){
+            secondmax = arr[i];
+        }
+    }
+    return secondmax;
+}
+
+bool sortedandrotated(vector<int>arr){
+    int count =0;
+    for(int i =0;i<arr.size()-1;i++){
+        if(arr[i]>arr[i+1%arr.size()])count++;
+    }
+    return count<=1;
+}
+
 int main(){
-    vector<int>arr = {1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,100,120};
-    cout<<"Found at index: "<<binarySearch(arr,20);
+    vector<int>arr = {10,12,15,1,2,20};
+    // cout<<"Found at index: "<<binarySearch(arr,20);
+    cout<<"sorted and rotated or not:"<<sortedandrotated(arr);
 
     return 0;
 }
