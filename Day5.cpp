81f18e5b41e060812e228e8a7eff1cd8da179d77
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-23 08:59:58 +0530

    Reversing

--- Day5.cpp
+++ Day5.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(){
-    int arr[] = {2,4,5,1,3};
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    vector<int>arr;
+    while(n){
+        arr.push_back(n%10);
+        n/=10;
+    }
+    cout<<"before reversing"<<endl;
+    for(int i =0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i =0;i<arr.size()/2;i++){
+        swap(arr[i],arr[arr.size()-1-i]);
+    }
+    cout<<"after reversing"<<endl;
+    for(int i: arr){
+        cout<<i<<" ";
+    }
+
 }
