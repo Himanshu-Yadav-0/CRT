28e3ee625d4546dc74c7f7e3c623ff25eae32c87
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-03-25 09:58:40 +0530

    operator overloading

--- Day7.cpp
+++ Day7.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+
+class isha{
+    public:
+    int a;
+    isha operator +(isha obj){
+        isha result;
+        result.a = a+obj.a;
+        return result;
+    }
+};
+
 int main(){
-    
+    isha himanshu,mota;
+    himanshu.a = 10;
+    mota.a = 20;
+    isha temp = himanshu + mota;
+    cout<<temp.a;
     return 0;
 }
